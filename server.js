01005a9f7f6fbffcd1ee7e5d3e23df1bd27b8b59
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-11 11:36:06 +0100

    Use express.static() to serve all the files in 'public'

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ import express from 'express'
 
 const app=express()
 
+/*
+Challenge:
+    1. Use express.static() to serve all the files in 'public'.
+*/
+
 
 const PORT=8000
 
-app.get("/",(req,res)=>{
-    res.send('Hello World! My first Express server is alive! 🎉');
-})
+app.use(express.static('public'))
 
 
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
-})
+}).on('error', (err) => {
+  console.error('Failed to start server:', err)
+}) 

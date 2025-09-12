0315e11ce46547de42e4fe348a4227d75e5faf6d
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-12 12:00:39 +0100

    feat: imprpve writing

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,7 @@
 import express from 'express'
-
-
-import {getGenres,getProducts} from '../controllers/productsControllers.js'
-
-
-
+import { getGenres, getProducts } from '../controllers/productsControllers.js'
 
 export const productsRouter = express.Router()
 
-productsRouter.get("/products",getProducts)
-productsRouter.get("/products/genres",getGenres)
+productsRouter.get('/genres', getGenres)
+productsRouter.get('/', getProducts)
--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 import express from 'express'
-import {productsRouter} from "./routes/products.js"
+import { productsRouter } from './routes/products.js'
 
-
-const app=express()
-const PORT=8000
-
-
-
-
+const app = express()
+const PORT = 8000
+ 
 app.use(express.static('public'))
 
-app.use("/api",productsRouter)
-
-
-
-
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.use('/api/products', productsRouter)
+ 
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
 }).on('error', (err) => {
   console.error('Failed to start server:', err)
 }) 

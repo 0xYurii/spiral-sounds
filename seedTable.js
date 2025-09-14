7db7242fa159f1701a2a0f98379dc595c0b5959f
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-14 10:25:18 +0100

    feat: update to look for the database file in the /tmp directory.

--- seedTable.js
+++ seedTable.js
@@ -4,39 +4,32 @@ import path from 'node:path'
 import { vinyl } from './data.js'
 
 async function seedTable() {
- 
   const db = await open({
-    filename: path.join('database.db'),
+    // Point to the /tmp directory here as well
+    filename: path.join('/tmp', 'database.db'),
     driver: sqlite3.Database
   })
 
   try {
-
     await db.exec('BEGIN TRANSACTION')
 
     for (const { title, artist, price, image, year, genre, stock } of vinyl) {
-
-      await db.run(`
+      await db.run(
+        `
         INSERT INTO products (title, artist, price, image, year, genre, stock)
         VALUES (?, ?, ?, ?, ?, ?, ?)`,
         [title, artist, price, image, year, genre, stock]
-        )
-
+      )
     }
 
     await db.exec('COMMIT')
     console.log('All records inserted successfully.')
-
   } catch (err) {
-
     await db.exec('ROLLBACK')
     console.error('Error inserting data:', err.message)
-
   } finally {
-
     await db.close()
     console.log('Database connection closed.')
-    
   }
 }
 

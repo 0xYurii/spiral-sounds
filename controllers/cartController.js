6b562c232c64f5e0ccea6bff5fdd23fc6ae6f4ee
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-30 22:11:00 +0100

    feat: add getAll endpoint to cart routes and clean up getCartCount function

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -24,14 +24,50 @@ export async function addToCart(req, res) {
 }
 
 export async function getCartCount(req, res) {
-  const db = await getDBConnection();
-  const result = await db.get('SELECT SUM(quantity) AS totalItems FROM cart_items WHERE user_id = ?', [req.session.userId])
-  res.json({ totalItems: result?.totalItems || 0 })
+  const db = await getDBConnection()
+
+  const result = await db.get(`SELECT SUM(quantity) AS totalItems FROM cart_items WHERE user_id = ?`, [req.session.userId])
+
+  res.json({ totalItems: result.totalItems || 0 })
+}  
+
+
+export async function getAll(req, res) {
+
+// Don't touch this code!
+  if (!req.session.userId) {
+    return res.json({err: 'not logged in'})
+  }
+
+  const db = await getDBConnection()
+  const items=await db.all('SELECT CI.id AS cartItemId,CI.quantity,P.title,P.artist,P.price FROM cart_items CI JOIN products P ON CI.product_id=P.id WHERE user_id = ?',[req.session.userId])
+  res.json({ items: items })
+
+
+
 /*
-Challenge:
+Challenge: 
 
-1. Write code to ensure that when a logged-in user clicks 'Add to Cart', their current cart count is shown in the header with a cart icon. The frontend has been done for you. All the backend need do is provide the following JSON on the /api/cart/cart-count endpoint: 
+1. When a logged-in user clicks the cart icon, they will be redirected to the cart.html page. To render the user's cart, the frontend needs to get an array of objects similar to the example below when it makes a GET request to the /api/cart endpoint. Important: this array should be sent in a JSON object with the key 'items'.
 
+[
+  {
+    cartItemId: 4,
+    quantity: 2,
+    title: 'Selling Dogma',
+    artist: 'The Clouds',
+    price: 44.99
+  },
+  {
+    cartItemId: 5,
+    quantity: 1,
+    title: 'Midnight Parallels',
+    artist: 'Neon Grove',
+    price: 40.99
+  }
+]
+
+The frontend JS has been done for you.
 
 Ignore frontend console errors for now!
  
@@ -39,9 +75,8 @@ For testing, log in with:
 Username: test
 Password: test
 
+Then click the cart icon to go to the cart page. You should see the user’s items.
+
 Loads of help in hint.md
 */
-
-  
-
-}  
+} 
